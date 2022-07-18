6ed56efd7ba8dbc0c6767982d4d165f27c0ff0a5
The Level Three Chin <henchelsantillan@gmail.com>
2022-07-17 23:43:09 -0400

    Update gameLogic.h

--- gameLogic.h
+++ gameLogic.h
@@ -9,6 +9,18 @@
 #include <cmsis_os2.h>
 #include <os_tick.h>
 
+typedef struct { // Struct to store the (x, y) coordinates of in-game sprites
+  uint32_t x;
+  uint32_t y;
+} Pos;
+
+typedef struct {  // The ball the player interacts with in-game is a point mass
+  float x_velocity;    // Get speed and direction information from these fields
+  float y_velocity;    // Velocity information is only used for the ball and not the hole
+  Pos pos;
+  char *bitmap;
+} Actor;
+
 void initLEDs(void);
 void updateLEDs(void *args);
 void readPowerInput(void *args);
@@ -17,8 +29,17 @@ void hitBall(void *args);
 void launchBall(void *args);
 void writeGolfScore(void *args);
 
-double convertAngle(double rawAngle);
+double convertAngle(uint32_t rawAngle);
 
-void initializeActors(void);
+void initActors(void);
+
+void draw(void *args);
+void drawPixelsAt(int x, int y, int limit);
+void drawSpriteAt(int x, int y, char *bitmap, int bitmap_size);
+
+void drawBall(void);
+
+void animate(void *args);
+bool inHole(Actor *ball, Actor *hole, int ball_size, int hole_size);
 
 #endif
