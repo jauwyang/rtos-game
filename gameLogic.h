080a346a03b6f900b0e529f576cd6a94a84512ea
The Level Three Chin <henchelsantillan@gmail.com>
2022-07-21 13:05:07 -0400

    Update gameLogic.h

--- gameLogic.h
+++ gameLogic.h
@@ -9,12 +9,13 @@
 #include <cmsis_os2.h>
 #include <os_tick.h>
 
-typedef struct { // Struct to store the (x, y) coordinates of in-game sprites
+// Struct to store the (x, y) coordinates of in-game sprites
+typedef struct { 
   int x;
   int y;
 } Pos;
 
-
+// Struct for the Ball sprite
 typedef struct {
 	Pos pos;
 	Pos arrowPos[2];
@@ -28,39 +29,38 @@ typedef struct {
 	
 } Ball;
 
-
+// Struct to store environment objects, i.e. the Hole and the Teleporter
 typedef struct {
 	Pos pos;
 	char *bitmap;
 	
-} Hole;
+} Environment;
 
-
+//***** LEDs and Power Mechanism *****//
 void initLEDs(void);
 void updateLEDs(void *args);
 void readPowerInput(void *args);
+
+//***** POTENTIOMETER and Angle Mechanism *****//
 void readDirectionInput(void *args);
+
+
+//***** PUSHBUTTON, Ball Control, and In-Game Functionality *****//	
 void hitBall(void *args);
 void launchBall(void);
-void writeGolfScore(void *args);
+void teleportBall(void *args);
+bool inTeleporter(int sizeBall, int sizeTeleporter);
+
+//***** SETUP, LOSE AND WIN CONDITION *****//
 void setupGame(void);
+void writeGolfScore(void *args);
+void checkEndGame(void *args);
+bool inHole(int ball_size, int hole_size);
 
-double convertAngle(uint32_t rawAngle);
-
-
+//***** DRAWING and HELPER FUNCTIONS *****//
 void drawPixelsAt(int x, int y, int limit);
 void drawSpriteAt(int x, int y, char *bitmap, int bitmap_size);
-
-void drawBall(void);
-
-void drawArrow(double angle, int centerX, int centerY);
-
-void createArrowBitMap(void *args);
+double convertAngle(int32_t rawAngle);
 uint32_t convertBinaryArrayToDecimal(uint32_t *bits, uint32_t arraySize);
-
-void checkEndGame(void *args);
-
-void animate(void *args);
-bool inHole(int ball_size, int hole_size);
 
 #endif
